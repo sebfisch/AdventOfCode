aeba044293d4dd50db2773c1ab6f959390fdcc53
Sebastian Fischer <mail@sebfisch.de>
2023-12-04 13:28:33 +0100

    day 4 part 1

---
+++ year2023/day04/task1.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+const stdout = std.io.getStdOut();
+var stdin = std.io.bufferedReader(std.io.getStdIn().reader());
+
+const Numbers = struct {
+    set: std.bit_set.StaticBitSet(100),
+
+    fn fromString(str: []const u8) !Numbers {
+        var result = Numbers{
+            .set = std.bit_set.StaticBitSet(100).initEmpty(),
+        };
+        var nums = std.mem.splitSequence(u8, str, " ");
+        while (nums.next()) |num| {
+            if (num.len > 0) { // skip multiple spaces
+                result.set.set(try std.fmt.parseInt(usize, num, 10));
+            }
+        }
+        return result;
+    }
+};
+
+const Card = struct {
+    winning: Numbers,
+    mine: Numbers,
+
+    fn fromString(str: []const u8) !Card {
+        var parts = std.mem.splitSequence(u8, str, " | ");
+        const winning = try Numbers.fromString(parts.next().?);
+        const mine = try Numbers.fromString(parts.next().?);
+        return Card{
+            .winning = winning,
+            .mine = mine,
+        };
+    }
+
+    fn pointValue(self: *Card) usize { // mutates self
+        self.winning.set.setIntersection(self.mine.set);
+        const matching: u4 = @intCast(self.winning.set.count());
+        const one: u32 = 1;
+        return if (matching == 0) 0 else (one << (matching - 1));
+    }
+};
+
+pub fn main() !void {
+    var points: usize = 0;
+
+    var buf: [1024]u8 = undefined;
+    while (try stdin.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var parts =
+            std.mem.splitSequence(u8, line, ": ");
+        _ = parts.next(); // skip the first part
+        var card = try Card.fromString(parts.next().?);
+        points += card.pointValue();
+    }
+
+    _ = try stdout.writer().print("{d}\n", .{points});
+}
